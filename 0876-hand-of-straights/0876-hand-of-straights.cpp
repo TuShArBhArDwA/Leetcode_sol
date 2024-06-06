afdd7843abe2a628665b00cc77fca62f5c12bd66
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2024-06-06 21:03:05 +0530

    Time: 402 ms (5.03%), Space: 23.1 MB (98.2%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-   bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
-        int next = hand[i] + 1;
-        hand[i] = -1;  // Mark as used
-        int count = 1;
-        i += 1;
-        while (i < n && count < groupSize) {
-            if (hand[i] == next) {
-                next = hand[i] + 1;
-                hand[i] = -1;
-                count++;
-            }
-            i++;
-        }
-        return count == groupSize;
-    }
-
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if (n % groupSize != 0) return false;
-        std::sort(hand.begin(), hand.end());
-        for (int i = 0; i < n; i++) {
-            if (hand[i] >= 0) {
-                if (!findSuccessors(hand, groupSize, i, n)) return false;
+       int first;
+    int count;
+    if(hand.size()%groupSize!=0){
+        return false;
+    }
+    sort(hand.begin(),hand.end());
+    for(int j=0;j<hand.size()/groupSize;j++){
+        for(int i=0;i<hand.size();i++){
+            if(hand.at(i)==-1){
+                continue;
+            }
+            else{
+                first=hand.at(i);
+                hand.at(i)=-1;
+                break;
             }
         }
-        return true;
+        count=1;
+        for(int j=0;j<hand.size();j++){
+            if(hand.at(j)==(first+1)){
+                count++;
+                first++;
+                hand.at(j)=-1;
+            }
+            if(count==groupSize){
+                break;
+            }
+        }
+        if(count!=groupSize){
+            return false;
+        }
+    }
+    return true;
     }
 };
