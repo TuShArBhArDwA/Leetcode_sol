e9db79971a2886b26eaa63b94c340c336759730e
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2024-07-07 10:40:46 +0530

    Time: 0 ms (100%), Space: 7.3 MB (30.43%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,6 +1,10 @@
+#pragma GCC optimize("Ofast")     
 class Solution {
 public:
-    int numWaterBottles(int numBottles, int numExchange) {
+    int numWaterBottles(int numBottles, int numExchange) { 
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         int ans=0;
         int extra=0;
         while(numBottles!=0){
@@ -9,8 +13,7 @@ public:
             numBottles/=numExchange;
             if(extra>=numExchange){
                 numBottles+=extra/numExchange;
-                extra%=numExchange;
-                
+                extra%=numExchange;     
             }
         }
         return ans;
