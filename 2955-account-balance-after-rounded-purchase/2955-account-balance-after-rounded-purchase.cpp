179884233e279aa65ef895d8a6b5f900fc09a853
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2024-07-07 11:09:16 +0530

    Time: 2 ms (53.18%), Space: 7.3 MB (36.39%) - LeetHub

--- 2955-account-balance-after-rounded-purchase/2955-account-balance-after-rounded-purchase.cpp
+++ 2955-account-balance-after-rounded-purchase/2955-account-balance-after-rounded-purchase.cpp
@@ -1,6 +1,10 @@
+#pragma GCC optimize("Ofast")    
 class Solution {
 public:
     int accountBalanceAfterPurchase(int purchaseAmount) {
+        cin.tie(0);
+        cout.tie(0);
+        ios::sync_with_stdio(false);
         if(purchaseAmount%10>4){
             purchaseAmount+=(10-purchaseAmount%10);
         }
