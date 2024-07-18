5a18a65df0fc86adac9d043d4741fc253d1e195e
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2024-07-18 19:00:29 +0530

    Time: 0 ms (100%), Space: 9.6 MB (62.9%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(int col,int n,vector<vector<string>>&ans,vector<string>&board,vector<int>&leftrow,vector<int>&upperdiag,vector<int>&lowerdiag){
+    void solve(int col,int n,vector<vector<string>>&ans,vector<string>&board,vector<int>&leftrow,vector<int>&lowerdiag,vector<int>&upperdiag){
         if(col==n){
             ans.push_back(board);
             return;
@@ -9,13 +9,13 @@ public:
             if(leftrow[row]==0&&upperdiag[n-1+col-row]==0&&lowerdiag[row+col]==0){
                 board[row][col]='Q';
                 leftrow[row]=1;
-                upperdiag[n-1+col-row]=1;
                 lowerdiag[row+col]=1;
-                solve(col+1,n,ans,board,leftrow,upperdiag,lowerdiag);
-                board[row][col]='.';
+                upperdiag[n-1+col-row]=1;
+                solve(col+1,n,ans,board,leftrow,lowerdiag,upperdiag);
+                 board[row][col]='.';
                 leftrow[row]=0;
-                upperdiag[n-1+col-row]=0;
                 lowerdiag[row+col]=0;
+                upperdiag[n-1+col-row]=0;
             }
         }
     }
@@ -26,8 +26,8 @@ public:
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        vector<int>leftrow(n,0),upperdiag(2*n-1,0),lowerdiag(2*n-1,0);
-        solve(0,n,ans,board,leftrow,upperdiag,lowerdiag);
+        vector<int>leftrow(n,0),lowerdiag(2*n-1,0),upperdiag(2*n-1,0);
+        solve(0,n,ans,board,leftrow,lowerdiag,upperdiag);
         return ans;
     }
 };
