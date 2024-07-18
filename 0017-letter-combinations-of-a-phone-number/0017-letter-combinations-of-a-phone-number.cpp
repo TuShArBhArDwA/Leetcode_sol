1b70f42ada1a59bfa3ec12d2af8edbb396b84b45
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2024-07-18 22:58:22 +0530

    Time: 3 ms (38.27%), Space: 8.6 MB (20.58%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    void solve(int index,string digits,vector<string>&ans,string temp,vector<string>&mpp){
-        if(index==digits.size()){
+    void solve(int ind,string digits,vector<string>&ans,string temp,vector<string>&mpp){
+        if(ind==digits.size()){
             ans.push_back(temp);
             return;
         }
-        string value=mpp[digits[index]-'0'];
+        string value=mpp[digits[ind]-'0'];
         for(char c:value){
             temp.push_back(c);
-            solve(index+1,digits,ans,temp,mpp);
+            solve(ind+1,digits,ans,temp,mpp);
             temp.pop_back();
         }
     }
@@ -17,8 +17,8 @@ public:
             return {};
         }
         vector<string>ans;
-        vector<string>mpp={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         string temp;
+        vector<string>mpp={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};   
         solve(0,digits,ans,temp,mpp);
         return ans;
     }
